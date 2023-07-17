095f87e507002ea83eb59425f53f36da2491341f
Allison Serrano <allisonserrano171@yahoo.com>
2023-07-17 15:31:55 -0400

    installed packages needed for this project and drafted the tables)

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE employees_db;
+
+--Will be using the database
+--USE employees_db;
+
+--Creating department table
+--CREATE TABLE department(
+--    id INT NOT NULL PRIMARY KEY,
+--    name VARCHAR(30)
+--)
+
+--Creating role table
+--CREATE TABLE role(
+--     id INT NOT NULL PRIMARY KEY,
+--     title VARCHAR(30),
+--     salary DECIMAL,
+--     FOREIGN KEY (department_id)
+--     REFRENCES department(id)
+--)
+
+--Creating employee table
+--CREATE TABLE employee(
+--      id INT NOT NULL PRIMARY KEY,
+--      first_name VARCHAR(30),
+--      last_name VARCHAR(30),
+--      FOREIGN KEY (role_id)
+--      REFRENCES role(id) 
+--)
