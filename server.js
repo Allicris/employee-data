5786b3cd71563ef1d190f8c511748a62e329b82c
Allison Serrano <allisonserrano171@yahoo.com>
2023-07-25 22:32:17 -0400

    added inquirer prompt function and edited list

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection(
 // Connecting to the database
 connection.connect((err) => {
     if (err) throw err;
-    console.log("Connected to the database!");
+    console.log("Connected to the database");
     // starting the application
     start();
 });
@@ -29,71 +29,103 @@ const questions = [
         type: "list",
         name: "promptStarter",
         message: "Hello, what would you like to see?",
-        choices: ["All Departments", "All Roles", "All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update Employee Role"]
-    },
-    //Inputting the new department name
-    {
-        type: "input",
-        name: "newDepartments",
-        message: "What is the department name?"
-    },
-    //Inputting a new role
-    {
-        type: "input",
+        choices: ["View Departments", "View Roles", "View Employees", "Add a Department", "Add a Role", "Add an Employee", "Update Employee Role", "Exit"]
+    }];
+
+function start() {
+    inquirer
+        .prompt(questions).then((answer) => {
+            switch (answer.promptStarter) {
+                case "View Departments": viewDepartments();
+                    break;
+                case "View Roles": viewRoles();
+                    break;
+                case "View Employees": viewEmployees();
+                    break;
+                case "Add a Department": newDepartment();
+                    break;
+                case "Add a Role": newRole();
+                    break;
+                case "Add an Employee": newEmployee();
+                    break;
+                case "Update Employee Role": updateEmployee();
+                    break;
+                case "Exit": connection.end();
+                console.log("Have a nice day!")
+                    break;
+            }
+        });
+}
+
+//Inputting the new department name
+function newDepartment() {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "departmentName",
+            message: "What is the department name?"
+}).then((answer) => {
+    console.log(answer.departmentName)
+    const query = 'INSERT INTO departments (departments_names VALUES ("${answer.DepartmentName}")';
+    connection.query(query, (err, res) => {
+        if (err)
+    })
+});
+//Inputting a new role
+{
+    type: "input",
         name: "newRoles",
-        message: "What is the role name?"
-    },
-    {
-        type: "input",
+            message: "What is the role name?"
+},
+{
+    type: "input",
         name: "newRoles",
-        message: "What is the salary for this role?"
-    },
-    {
-        type: "list",
+            message: "What is the salary for this role?"
+},
+{
+    type: "list",
         name: "newRoles",
-        message: "What department does this role belong to?",
-        choices: ["Accounting", "Sales", "Customer Service", "Technical Service"]
-    },
-    //Inputting a new employee
-    {
-        type: "input",
+            message: "What department does this role belong to?",
+                choices: ["Accounting", "Sales", "Customer Service", "Technical Service"]
+},
+//Inputting a new employee
+{
+    type: "input",
         name: "newEmployee",
-        message: "First name?"
-    },
-    {
-        type: "input",
+            message: "First name?"
+},
+{
+    type: "input",
         name: "newEmployee",
-        message: "Last name?"
-    },
-    {
-        type: "list",
+            message: "Last name?"
+},
+{
+    type: "list",
         name: "newEmployee",
-        message: "Role?",
-        choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"],
+            message: "Role?",
+                choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"],
     },
-    {
-        type: "input",
+{
+    type: "input",
         name: "newEmployee",
-        //How can I connect to an id in this question?
-        message: "Employee's Manager?"
-    },
-    {
-        type: "list",
+            //How can I connect to an id in this question?
+            message: "Employee's Manager?"
+},
+{
+    type: "list",
         name: "updateEmployee",
-        message: "Update Employee",
-        //How can I combine the first name and last name?
-        choices: ["first name, last name??"]
-    },
-    {
-        type: "list",
+            message: "Update Employee",
+                //How can I combine the first name and last name?
+                choices: ["first name, last name??"]
+},
+{
+    type: "list",
         name: "updateEmployee",
-        message: "Select Role",
-        choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"]
-    },
+            message: "Select Role",
+                choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"]
+},
 ]
 
-inquirer
-    .prompt(questions).then(response)
 //we want to trigger the data questions
 //would it be res.body.then the next const?
 
