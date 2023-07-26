c68613467ded60743dc6559f26b93ef6da7ea2b1
Allison Serrano <allisonserrano171@yahoo.com>
2023-07-25 23:02:07 -0400

    was able to make the functions view departments, roles and employees on the terminal

--- server.js
+++ server.js
@@ -3,25 +3,28 @@ const inquirer = require('inquirer');
 const mySQL = require('mysql2');
 
 // Making a connection with mySql credentials
-const db = mysql.createConnection(
+const connection = mySQL.createConnection(
     {
         host: 'localhost',
         // MySQL username,
         user: 'root',
         // MySQL password
         password: 'Alicris051!',
-        database: 'employee_db'
+        database: 'employees_db'
     },
-    console.log(`Connected to the employee_db database.`)
+    console.log(`Connected to mySQL and database!`)
 );
 
 // Connecting to the database
 connection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected to the database");
-    // starting the application
+if (err) {
+    console.log(err);
+    res.status(400).send(err);
+    }
+    // Officially start the application
     start();
 });
+
 
 //How can this data be placed in our database?
 const questions = [
@@ -56,77 +59,122 @@ function start() {
             }
         });
 }
-
-//Inputting the new department name
-function newDepartment() {
-    inquirer
-        .prompt({
-            type: "input",
-            name: "departmentName",
-            message: "What is the department name?"
-}).then((answer) => {
-    console.log(answer.departmentName)
-    const query = 'INSERT INTO departments (departments_names VALUES ("${answer.DepartmentName}")';
+//Pulling all of the departments from the database
+function viewDepartments() {
+    const query = "SELECT * FROM departments";
     connection.query(query, (err, res) => {
-        if (err)
-    })
-});
-//Inputting a new role
-{
-    type: "input",
-        name: "newRoles",
-            message: "What is the role name?"
-},
-{
-    type: "input",
-        name: "newRoles",
-            message: "What is the salary for this role?"
-},
-{
-    type: "list",
-        name: "newRoles",
-            message: "What department does this role belong to?",
-                choices: ["Accounting", "Sales", "Customer Service", "Technical Service"]
-},
-//Inputting a new employee
-{
-    type: "input",
-        name: "newEmployee",
-            message: "First name?"
-},
-{
-    type: "input",
-        name: "newEmployee",
-            message: "Last name?"
-},
-{
-    type: "list",
-        name: "newEmployee",
-            message: "Role?",
-                choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"],
-    },
-{
-    type: "input",
-        name: "newEmployee",
-            //How can I connect to an id in this question?
-            message: "Employee's Manager?"
-},
-{
-    type: "list",
-        name: "updateEmployee",
-            message: "Update Employee",
-                //How can I combine the first name and last name?
-                choices: ["first name, last name??"]
-},
-{
-    type: "list",
-        name: "updateEmployee",
-            message: "Select Role",
-                choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"]
-},
-]
+        if (err) {
+        console.log(err)
+        res.status(400).send(err);
+        }
+        //returning the table to the client
+        console.table(res);
+        // restart the application
+        start();
+    });
+}
 
-//we want to trigger the data questions
-//would it be res.body.then the next const?
+//Pulling all of the departments from the database
+function viewRoles() {
+    const query = "SELECT * FROM roles";
+    connection.query(query, (err, res) => {
+        if (err) {
+        console.log(err);
+        res.status(400).send(err);
+        }
+        //returning the table to the client
+        console.table(res);
+        // restart the application
+        start();
+    });
+}
+
+// function to view all employees
+function viewEmployees() {
+    const query = "SELECT * FROM employee";
+    connection.query(query, (err, res) => {
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+            }
+            //returning the table to the client
+            console.table(res);
+            // restart the application
+            start();
+        });
+    }
+
+// //Inputting the new department name
+// function newDepartment() {
+//     inquirer
+//         .prompt({
+//             type: "input",
+//             name: "departmentName",
+//             message: "What is the department name?"
+// }).then((answer) => {
+//     console.log(answer.departmentName)
+//     const query = 'INSERT INTO departments (departments_names VALUES ("${answer.DepartmentName}")';
+//     connection.query(query, (err, res) => {
+//         if (err)
+//         )
+// });
+// }
+// //Inputting a new role
+// {
+//     type: "input",
+//         name: "newRoles",
+//             message: "What is the role name?"
+// },
+// {
+//     type: "input",
+//         name: "newRoles",
+//             message: "What is the salary for this role?"
+// },
+// {
+//     type: "list",
+//         name: "newRoles",
+//             message: "What department does this role belong to?",
+//                 choices: ["Accounting", "Sales", "Customer Service", "Technical Service"]
+// },
+// //Inputting a new employee
+// {
+//     type: "input",
+//         name: "newEmployee",
+//             message: "First name?"
+// },
+// {
+//     type: "input",
+//         name: "newEmployee",
+//             message: "Last name?"
+// },
+// {
+//     type: "list",
+//         name: "newEmployee",
+//             message: "Role?",
+//                 choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"],
+//     },
+// {
+//     type: "input",
+//         name: "newEmployee",
+//             //How can I connect to an id in this question?
+//             message: "Employee's Manager?"
+// },
+// {
+//     type: "list",
+//         name: "updateEmployee",
+//             message: "Update Employee",
+//                 //How can I combine the first name and last name?
+//                 choices: ["first name, last name??"]
+// },
+// {
+//     type: "list",
+//         name: "updateEmployee",
+//             message: "Select Role",
+//                 choices: ["Manager", "Assistant Manager", "Office Administrator", "Sales Associate", "Customer Service Clerk", "Software Developer"]
+// },
+// ]
+
+// //we want to trigger the data questions
+// //would it be res.body.then the next const?
 
 
